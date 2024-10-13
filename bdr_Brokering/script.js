eee98ff13ab97da673b81df4432b1792fc5607c6
sbroke03 <sbroke03@gmail.com>
2024-10-13 17:40:35 -0500

    Add files via upload

---
+++ bdr_Brokering/script.js
@@ -0,0 +1,5 @@
+function changeText() {
+  document.getElementById("main-text").style.display = "none";
+  document.getElementById("go-button").style.display = "none";
+  document.getElementById("full-image").style.display = "block";
+}
